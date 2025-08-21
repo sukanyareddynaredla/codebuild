958fed4aa31ee05e6dd6daf179cb6dac0c8da5c2
Sukanya Reddy <sukanyareddynaredla@gmail.com>
2025-08-21 13:20:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React + AWS CodeBuild 🚀</h1>
+      <p>This is my first deployed website!</p>
+    </div>
+  );
+}
+
+export default App;
